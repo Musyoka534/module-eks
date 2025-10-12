358efdbeae22398bfa1380dc12eb55be84195057
Musyoka Kilonzo <musyokakilonzo@CP-Mac-Musyoka.local>
2025-10-12 15:54:55 +0300

    update code

--- outputs.tf
+++ outputs.tf
@@ -12,4 +12,16 @@ output "cluster_security_group_id" {
 output "oidc_provider_url" {
   value = aws_iam_openid_connect_provider.this[0].url
 }
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+output "cluster_token" {
+  value     = data.aws_eks_cluster_auth.this.token
+  sensitive = true
+}
+
 
